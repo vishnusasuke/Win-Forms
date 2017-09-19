147b4b81169c621e7c0db26a67e283dee2f58426
vishnusasuke <vishnusasuke@gmail.com>
2017-09-19 14:54:40 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class Class1
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
